a238297042b4465e0a92df925fbae0fddda6a352
yurim0227 <byurim227@gmail.com>
2023-06-14 16:59:12 +0900

    .

--- 20230614_multichat/src/kh/lclass/chat/server/ServerBackground.java
+++ 20230614_multichat/src/kh/lclass/chat/server/ServerBackground.java
@@ -72,9 +72,9 @@ public class ServerBackground {
 	}
 	
 	// 클라이언트 접속이 끊어지면 그 정보를 나타내 주는 메소드
-		public void removeClient(String nickname) {
-			gui.appendMsg(nickname+"님이 나가셨습니다.");
-		}
+	public void removeClient(String nickname) {
+		gui.appendMsg(nickname+"님이 나가셨습니다.");
+	}
 	// 클라이언트 접속하면 그 정보를 나타내 주는 메소드
 	public void addClient(String nickname) {
 		gui.appendMsg(nickname+"님이 접속했습니다.");
